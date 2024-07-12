021a0a39b0ccdd0c182b11ca78e8149e2824f715
Schild, Philippe <Philippe.Schild@adesso.de>
2024-07-12 20:47:17 +0200

    postgres config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5439,
+      username: 'root',
+      password: 'rootPW',
+      database: 'hoptimisten',
+      entities: ['**/*.entity{.ts,.js}'],
+      migrationsTableName: 'migration',
+      migrations: ['src/migration/*.ts'],
+      // cli: {
+      //   migrationsDir: 'src/migration',
+      // },
+      ssl: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
