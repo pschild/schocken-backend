db397763c9e4b05af41b35417609c6040eeaa209
Schild, Philippe <Philippe.Schild@adesso.de>
2024-08-12 21:03:56 +0200

    created GameEvent and EventType resources

--- src/player/player.service.integration.spec.ts
+++ src/player/player.service.integration.spec.ts
@@ -2,6 +2,7 @@ import { Test } from '@nestjs/testing';
 import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
 import { firstValueFrom } from 'rxjs';
 import { DataSource, Repository } from 'typeorm';
+import { EventType } from '../model/event-type.entity';
 import { GameEvent } from '../model/game-event.entity';
 import { Game } from '../model/game.entity';
 import { Round } from '../model/round.entity';
@@ -20,7 +21,7 @@ describe('PlayerService integration', () => {
   let repo: Repository<Player>;
 
   beforeAll(async () => {
-    source = await setupDataSource([Game, Round, Player, GameEvent]);
+    source = await setupDataSource([Game, Round, Player, GameEvent, EventType]);
 
     const moduleRef = await Test.createTestingModule({
       imports: [
