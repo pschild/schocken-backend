dacef1b57c859c75b5f2d94e4d461a39f8b60241
Schild, Philippe <Philippe.Schild@adesso.de>
2024-09-14 17:23:54 +0200

    added Spelunker package for generating a dependency graph on application start

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,21 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { AppModule } from './app.module';
+import { SpelunkerModule } from 'nestjs-spelunker';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const tree = SpelunkerModule.explore(app);
+  const root = SpelunkerModule.graph(tree);
+  const edges = SpelunkerModule.findGraphEdges(root);
+  const ignoredModules = ['ConfigModule', 'ConfigHostModule', 'WinstonModule', 'TypeOrmCoreModule', 'TypeOrmModule'];
+  const mermaidEdges = edges
+    .filter(({ from ,to }) => !ignoredModules.includes(from.module.name) && !ignoredModules.includes(to.module.name))
+    .map(({ from, to }) => `  ${from.module.name}-->${to.module.name}`);
+  console.log('graph');
+  console.log(mermaidEdges.join('\n'));
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   await app.listen(process.env.PORT || 3000);
