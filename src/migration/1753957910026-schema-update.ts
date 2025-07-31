7fec67720727f22567ba050e3a63d8b233207bd1
Schild, Philippe <Philippe.Schild@adesso.de>
2025-07-31 21:26:25 +0200

    fixed migration

--- src/migration/1753957910026-schema-update.ts
+++ src/migration/1753957910026-schema-update.ts
@@ -1,9 +1,10 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class SchemaUpdate1752575805401 implements MigrationInterface {
-    name = 'SchemaUpdate1752575805401'
+export class SchemaUpdate1753957910026 implements MigrationInterface {
+    name = 'SchemaUpdate1753957910026'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TYPE "public"."payment_penaltyunit_enum" AS ENUM('EURO', 'BEER_CRATE')`);
         await queryRunner.query(`CREATE TABLE "payment" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createDateTime" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "lastChangedDateTime" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "deletedDateTime" TIMESTAMP, "penaltyUnit" "public"."payment_penaltyunit_enum", "penaltyValue" real NOT NULL, "outstandingValue" real NOT NULL, "confirmed" boolean NOT NULL DEFAULT false, "confirmedAt" TIMESTAMP WITH TIME ZONE, "confirmedBy" character varying(64), "playerId" uuid NOT NULL, "gameId" uuid NOT NULL, CONSTRAINT "PK_fcaec7df5adf9cac408c686b2ab" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE INDEX "IDX_8de8a192d00e33568fc1e64463" ON "payment" ("playerId") `);
         await queryRunner.query(`CREATE INDEX "IDX_b014b2138aa30910989bfb6c31" ON "payment" ("gameId") `);
@@ -17,6 +18,7 @@ export class SchemaUpdate1752575805401 implements MigrationInterface {
         await queryRunner.query(`DROP INDEX "public"."IDX_b014b2138aa30910989bfb6c31"`);
         await queryRunner.query(`DROP INDEX "public"."IDX_8de8a192d00e33568fc1e64463"`);
         await queryRunner.query(`DROP TABLE "payment"`);
+        await queryRunner.query(`DROP TYPE "public"."payment_penaltyunit_enum"`);
     }
 
 }
